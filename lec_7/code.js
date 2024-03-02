48d41a19b38615e67d1cb5e11604af2c174e3a89
Harendra Pratap Singh <hk284088@gmail.com>
2024-03-02 14:25:41 +0530

    lec 10 done

---
+++ lec_7/code.js
@@ -0,0 +1,4 @@
+console.log("1"+2+2) // agr phle string hai to sare string ki trh treat hoge
+// output 122
+console.log(1+2+"2") // agr pahle numbers hai to phle vo add ho jaege fir bad me jab string aa yegi to fir sabhi string ki trh treat hoge
+// output 32 
