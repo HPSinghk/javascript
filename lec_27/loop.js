2fdfc12de9d315b68d7fcaf268c0e689adbe5bde
Harendra Pratap Singh <hk284088@gmail.com>
2024-03-03 23:59:33 +0530

    loop done

---
+++ lec_27/loop.js
@@ -0,0 +1,14 @@
+// for of 
+
+/* const arr =[1,2,3,4,5]
+for (const i of arr) {
+    console.log(i)  
+}
+
+const greeting = "hello world"
+for (const i of greeting) {
+    console.log(i)    
+} */
+
+//map
+const map = new Map()
